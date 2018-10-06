2f9153ce4ad3e67db81ce7b1ad3bccf2ea93aa94
Brian Gershon <briang@unity3d.com>
2018-10-05 17:01:00 -0700

    Simplify cluster config in sample app by grouping everything together.

--- main.go
+++ main.go
@@ -26,27 +26,23 @@ func main() {
 
 	tokenProvider := new(sampleBearerTokenProvider)
 
-	podListRetriever := &deploy.KubernetesPodListRetriever{
-		Client:             client,
-		Endpoint:           os.Getenv("KUBERNETES_ENDPOINT"),
-		Namespace:          os.Getenv("KUBERNETES_NAMESPACE"),
-		BearerTokenService: tokenProvider,
-	}
-
-	deployer := &deploy.KubernetesDeployer{
-		Client:             client,
-		Endpoint:           os.Getenv("KUBERNETES_ENDPOINT"),
-		Namespace:          os.Getenv("KUBERNETES_NAMESPACE"),
-		BearerTokenService: tokenProvider,
-		DeploymentName:     os.Getenv("KUBERNETES_DEPLOYMENT_NAME"),
-		ContainerName:      os.Getenv("KUBERNETES_DEPLOYMENT_CONTAINERNAME"),
-		ContainerImage:     os.Getenv("KUBERNETES_DEPLOYMENT_IMAGE_PREFIX"),
-	}
-
 	cluster := deploy.KubernetesClusterNamespace{
-		Description:  os.Getenv("DESCRIPTION"),
-		PodRetriever: podListRetriever,
-		DeployMaker:  deployer,
+		Description: os.Getenv("DESCRIPTION"),
+		PodRetriever: &deploy.KubernetesPodListRetriever{
+			Client:             client,
+			Endpoint:           os.Getenv("KUBERNETES_ENDPOINT"),
+			Namespace:          os.Getenv("KUBERNETES_NAMESPACE"),
+			BearerTokenService: tokenProvider,
+		},
+		DeployMaker: &deploy.KubernetesDeployer{
+			Client:             client,
+			Endpoint:           os.Getenv("KUBERNETES_ENDPOINT"),
+			Namespace:          os.Getenv("KUBERNETES_NAMESPACE"),
+			BearerTokenService: tokenProvider,
+			DeploymentName:     os.Getenv("KUBERNETES_DEPLOYMENT_NAME"),
+			ContainerName:      os.Getenv("KUBERNETES_DEPLOYMENT_CONTAINERNAME"),
+			ContainerImage:     os.Getenv("KUBERNETES_DEPLOYMENT_IMAGE_PREFIX"),
+		},
 	}
 
 	command, containerTag := pickCommand(os.Args)
